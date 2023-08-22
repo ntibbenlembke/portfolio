0e3ab35c9756b4dba727a0dd2aea1a6de74bc660
Noah Tibben-Lembke <ntibbenlembke@gmail.com>
2023-08-22 01:18:47 -0500

    resume section finalized

--- index.js
+++ index.js
@@ -131,3 +131,20 @@ const showMessage = (message, updateColor) => {
       })
     }, 5000)
   }
+
+
+document.body.addEventListener("mousemove", evt => {
+    const mouseX = evt.clientX;
+    const mouseY = evt.clientY;
+    
+    gsap.set(".cursor", {
+      x: mouseX,
+      y: mouseY
+    })
+    
+    gsap.to(".shape", {
+      x: mouseX,
+      y: mouseY,
+      stagger: -0.1
+    })
+  })
